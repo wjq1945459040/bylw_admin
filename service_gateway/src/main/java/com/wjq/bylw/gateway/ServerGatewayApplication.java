e9fa00771577ff971d1bf5ecd6af30350882be7f
wjq <1945459040@qq.com>
2022-02-28 22:53:45 +0800

    [新增]springcloud网关整合

---
+++ service_gateway/src/main/java/com/wjq/bylw/gateway/ServerGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.wjq.bylw.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wjq
+ * @date 2022/02/28
+ **/
+
+@SpringBootApplication
+public class ServerGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerGatewayApplication.class, args);
+    }
+}
---
+++ service_gateway/src/main/java/com/wjq/bylw/gateway/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.wjq.bylw.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @author wjq
+ * @date 2022/02/24 16:42
+ **/
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
