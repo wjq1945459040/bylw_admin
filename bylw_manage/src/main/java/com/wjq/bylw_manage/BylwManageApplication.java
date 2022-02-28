146152d254541d7d6ba86e77a4d00b4803b09605
wjq <1945459040@qq.com>
2022-02-28 15:53:17 +0800

    [新增]用户操作对应的后台

---
+++ bylw_manage/src/main/java/com/wjq/bylw_manage/BylwManageApplication.java
@@ -0,0 +1,13 @@
+package com.wjq.bylw_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BylwManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BylwManageApplication.class, args);
+    }
+
+}
---
+++ bylw_manage/src/test/java/com/wjq/bylw_manage/BylwManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjq.bylw_manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BylwManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
